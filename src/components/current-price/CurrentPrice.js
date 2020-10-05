e3cd4c3038269a0582992c693ec69ac808c039e9
CallumBuchanan-1 <callum.buchanan@aquaq.co.uk>
2020-10-05 11:49:35 +0100

    Drop down updated

--- src/components/current-price/CurrentPrice.js
+++ src/components/current-price/CurrentPrice.js
@@ -1,87 +1,93 @@
-import React, { useState } from 'react';
-import Select from 'react-select';
+import React, { Component } from 'react'
+import Select from 'react-select'
+import axios from 'axios'
 
-function App() {
-  const data = [
-    {
-      value: 1,
-      label: "APPLE"
-    },
-    {
-      value: 2,
-      label: "AIG"
-    },
-    {
-      value: 3,
-      label: "AMD"
-    },
-    {
-      value: 4,
-      label: "DELL"
-    },
-    {
-      value: 5,
-      label: "DOW"
-    },
-    {
-      value: 6,
-      label: "GOOGLE"
-    },
-    {
-        value:7,
-        label: "HPQ"
-    },
-    {
-        value:8,
-        label:"IBM"
-    },
-    {
-        value:9,
-        label:"INTC"
-    },
-    {
-        value:10,
-        label:"MICROSOFT"
-    },
-  ];
-  const current = new Date();
-  const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
-  const yday = current.setDate(current.getDate()-1);
 
-  const [selectedOption, setSelectedOption] = useState(null);
- 
-  // handle onChange event of the dropdown
-  const handleChange = e => {
-    setSelectedOption(e);
+export default class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      selectOptions : [],
+      sym: "",
+      price: '',
+      price1: '',
+      price2: ''
+    }
   }
- 
-  return (
-    <div className="App">
- 
-      <Select
-        placeholder="Select Option"
-        value={selectedOption} // set selected value
-        options={data} // set list of the data
-        onChange={handleChange} // assign onChange function
-      />
- 
-      {selectedOption && <div style={{ marginTop: 20, lineHeight: '25px' }}>
-        <b>Trading Info for {date}:</b><br />
-        <div style={{ marginTop: 10 }}><b>Sym: </b> {selectedOption.label}</div>
-        <div><b>Max Trade Price: </b> {selectedOption.value}</div>
-        <div><b>Min Trade Price: </b> {selectedOption.value}</div>
-        <div><b>Current Trade Price: </b> {selectedOption.value}</div>
-        <p></p>
-        <b>{yday} Trading Info:</b>
-        <div style={{ marginTop: 10 }}><b>Sym: </b> {selectedOption.label}</div>
-        <div><b>Max Trade Price: </b> {selectedOption.value}</div>
-        <div><b>Min Trade Price: </b> {selectedOption.value}</div>
-        <div><b>Closing Trade Price: </b> {selectedOption.value}</div>
-        <p></p>
 
-      </div>}
-    </div>
-  );
+  async getOptions() { 
+    //Async/Await Query to qRest here
+    //Then put results in state's graphData
+
+    var url="https://81.150.99.19:8035/executeQuery";
+
+  
+    let queryRequest= {
+        "query": "(select last price, max price, min price by sym from trade)",
+        "type": "sync",
+        "response": true
+    };
+
+    let response = await axios.post(url, queryRequest, {
+        headers: {
+            "Accept": "*/*",
+            "Authorization": "BASIC dXNlcjpwYXNz"
+        },
+        auth: {
+            username: 'user',
+            password: 'pass'
+        }
+    });
+
+    console.log(response);
+
+    //function myFunction(item, index, arr) {
+    //  arr[index] = item.sym;
+    //}
+  
+    let res = response.data.result;
+    //res.forEach(myFunction);
+
+    //alert(res);
+    
+
+    //window.setInterval(function () {
+      //  this.updateDateTime();
+      //}.bind(this), 1000);
+
+      const options = res.map(d => ({
+        "label" : d.sym,
+        "value" : d.price,
+        "value2": d.price1,
+        "value3": d.price2}))
+  
+  this.setState({selectOptions: options})
+  
+  } 
+
+  handleChange(e){
+   this.setState({sym:e.label,
+    currentprice:e.value, 
+    maxprice:e.value2, 
+    minprice:e.value3})
+  }
+
+  componentDidMount(){
+    this.getOptions()
+  }
+  
+  render() {
+    console.log(this.state.selectOptions)
+    return (
+      <div>
+        <Select options={this.state.selectOptions} 
+        onChange={this.handleChange.bind(this)} />
+    <p>You have selected <strong>{this.state.sym}</strong></p>
+    <p>Current price is <strong>{this.state.currentprice}</strong></p>
+    <p>Max price is <strong>{this.state.maxprice}</strong></p>
+    <p>Min price is <strong>{this.state.minprice}</strong></p>
+      </div>
+    )
+  }
 }
-
-export default App;
