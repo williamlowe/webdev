1f469d24da5efdf290afcf10a6de1fe4cefc66cc
jking-aq <Joseph.King@aquaq.co.uk>
2020-10-05 07:19:15 -0400

    Working Timeseries Line Graph w/ Slider

--- src/components/timeseries/Timeseries.js
+++ src/components/timeseries/Timeseries.js
@@ -1,6 +1,7 @@
 import * as React from 'react';  
 import { Slider } from '@material-ui/core';
 import axios from 'axios';
+import Chart from 'react-apexcharts';
 
 export default class Timeseries extends React.Component {
     constructor(props) {  
@@ -21,13 +22,168 @@ export default class Timeseries extends React.Component {
             tt = tt + x;
         }
 
-        this.state = {  
-            graphData: {},
+        let today = new Date();
+        let hours = today.getUTCHours();
+        let minutes = today.getUTCMinutes();
+        let cTime = Math.ceil((hours*60+minutes)/15)
+
+        this.state = {
             currentDate: kdbDate,
             currentTime: kdbTime,
             rangeShort: 0,
-            rangeLong: 12,
-            sliderLabels: times
+            rangeLong: cTime,
+            sliderLabels: times,
+            series: [{
+                name: "AAPL",
+                data: []
+              },
+              {
+                name: "AIG",
+                data: []
+              },
+              {
+                name: "AMD",
+                data: []
+              },
+              {
+                name: "DELL",
+                data: []
+              },
+              {
+                name: "DOW",
+                data: []
+              },
+              {
+                name: "GOOG",
+                data: []
+              },
+              {
+                name: "HPQ",
+                data: []
+              },
+              {
+                name: "IBM",
+                data: []
+              },
+              {
+                name: "INTC",
+                data: []
+              },
+              {
+                name: "MSFT",
+                data: []
+              }
+            ],
+            options: {
+                chart: {
+                  height: 400,
+                  type: 'line',
+                  zoom: {
+                    enabled: false
+                  },
+                },
+                dataLabels: {
+                  enabled: false
+                },
+                stroke: {
+                  curve: 'straight'
+                },
+                title: {
+                  text: 'Running Average Price',
+                  align: 'left'
+                },
+                legend: {
+                  tooltipHoverFormatter: function(val, opts) {
+                    return val + ' - ' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + ''
+                  }
+                },
+                markers: {
+                  size: 0,
+                  hover: {
+                    sizeOffset: 6
+                  }
+                },
+                xaxis: {
+                  categories: times.slice(0, cTime+1)
+                },
+                tooltip: {
+                  y: [
+                    {
+                      title: {
+                        formatter: function (val) {
+                          return val;
+                        }
+                      }
+                    },
+                    {
+                      title: {
+                        formatter: function (val) {
+                          return val;
+                        }
+                      }
+                    },
+                    {
+                      title: {
+                        formatter: function (val) {
+                          return val;
+                        }
+                      }
+                    },
+                    {
+                        title: {
+                            formatter: function (val) {
+                            return val;
+                            }
+                        }
+                    },
+                    {
+                        title: {
+                          formatter: function (val) {
+                            return val;
+                          }
+                        }
+                    },
+                    {
+                        title: {
+                          formatter: function (val) {
+                            return val;
+                          }
+                        }
+                    },
+                    {
+                        title: {
+                          formatter: function (val) {
+                            return val;
+                          }
+                        }
+                    },
+                    {
+                        title: {
+                          formatter: function (val) {
+                            return val;
+                          }
+                        }
+                    },
+                    {
+                        title: {
+                          formatter: function (val) {
+                            return val;
+                          }
+                        }
+                    },
+                    {
+                        title: {
+                          formatter: function (val) {
+                            return val;
+                          }
+                        }
+                    }
+                  ]
+                },
+                grid: {
+                  borderColor: '#f1f1f1',
+                }
+            }
         }  
         this.updateRange = this.updateRange.bind(this);
         this.getLabel = this.getLabel.bind(this);
@@ -83,15 +239,12 @@ export default class Timeseries extends React.Component {
         return this.state.sliderLabels[index];
     }
 
-    async componentDidMount() { 
-        //Async/Await Query to qRest here
-        //Then put results in state's graphData
-
+    async getData(){
         var url="https://81.150.99.19:8035/executeQuery";
 
       
         let queryRequest= {
-            "query": "(select distinct sym from trade)",
+            "query": "(select .Q.f[3; avg(avgs price)] by sym, 15 xbar time.minute from trade where time.date = .z.d, time.minute within("+this.state.sliderLabels[this.state.rangeShort]+";"+this.state.sliderLabels[this.state.rangeLong]+"))",
             "type": "sync",
             "response": true
         };
@@ -109,20 +262,107 @@ export default class Timeseries extends React.Component {
 
         console.log(response);
 
-
-        function myFunction(item, index, arr) {
-            arr[index] = item.sym;
-        }
-        
+        let AAPLp=[], AIGp=[], AMDp=[], DELLp=[], DOWp=[], GOOGp=[], HPQp=[], IBMp=[], INTCp=[], MSFTp=[];
         let res = response.data.result;
-        res.forEach(myFunction);
+        let max = res.length;
+        for(let i=0; i<max; i++){
+            switch (res[i].sym){
+                case "AAPL":
+                    AAPLp.push(res[i].price);
+                    break;
+                case "AIG":
+                    AIGp.push(res[i].price);
+                    break;
+                case "AMD":
+                    AMDp.push(res[i].price);
+                    break;
+                case "DELL":
+                    DELLp.push(res[i].price);
+                    break;
+                case "DOW":
+                    DOWp.push(res[i].price);
+                    break;
+                case "GOOG":
+                    GOOGp.push(res[i].price);
+                    break;
+                case "HPQ":
+                    HPQp.push(res[i].price);
+                    break;
+                case "IBM":
+                    IBMp.push(res[i].price);
+                    break;
+                case "INTC":
+                    INTCp.push(res[i].price);
+                    break;
+                case "MSFT":
+                    MSFTp.push(res[i].price);
+                    break;
+                default:
+                    break;
+            }
+        }
 
-        alert(res);
+        let newSeries = [{
+            name: "AAPL",
+            data: AAPLp
+          },
+          {
+            name: "AIG",
+            data: AIGp
+          },
+          {
+            name: "AMD",
+            data: AMDp
+          },
+          {
+            name: "DELL",
+            data: DELLp
+          },
+          {
+            name: "DOW",
+            data: DOWp
+          },
+          {
+            name: "GOOG",
+            data: GOOGp
+          },
+          {
+            name: "HPQ",
+            data: HPQp
+          },
+          {
+            name: "IBM",
+            data: IBMp
+          },
+          {
+            name: "INTC",
+            data: INTCp
+          },
+          {
+            name: "MSFT",
+            data: MSFTp
+          }
+        ];
+
+        let newOptions = this.state.options;
+
+        newOptions.xaxis = this.state.sliderLabels.slice(this.state.rangeShort, this.state.rangeLong);
+
+        this.setState({series: newSeries, options:newOptions});
+    }
+
+    async componentDidMount() { 
         
+        await this.getData();
 
         window.setInterval(function () {
             this.updateDateTime();
           }.bind(this), 1000);
+
+          window.setInterval(function () {
+            this.getData();
+          }.bind(this), 2000);
+
     } 
 
     render() {  
@@ -143,7 +383,9 @@ export default class Timeseries extends React.Component {
         return (  
             <div>
                 <p>Timeseries Graph and Slider for {this.state.currentDate}D{this.state.currentTime}</p>
-                <br/><br/>
+                <br/>
+                <Chart options={this.state.options} series={this.state.series} type="line" />
+                <br/>
                 <Slider
                     defaultValue={[0, Math.ceil((hh*60+mm)/15)]}
                     onChangeCommitted={this.updateRange}
